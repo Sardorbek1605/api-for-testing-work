f8668bc60bbb09775b5c6899285385ecc32a20d7
Sardor Abduqodirov <abduqodirov.sardor1605@gmail.com>
2022-02-18 22:00:39 +0500

    finished

--- api-for-testing-work/routes/api.php
+++ api-for-testing-work/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::group(['middleware' => ['jwt.verify']], function() {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::post('/me', [AuthController::class, 'me']);
+    });
+});
+
+Route::group(['middleware' => ['api', 'jwt.verify']], function () {
+    Route::get('products', [ApiController::class, 'products']);
+    Route::post('save-card', [ApiController::class, 'save_to_card']);
+    Route::post('pay', [ApiController::class, 'pay']);
 });
